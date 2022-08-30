c27f0066a03803970165c5529eff6a237934969e
unknown <jos.espinozav@duocuc.cl>
2022-08-29 20:57:05 -0400

    hola3

--- blank/src/app/app-routing.module.ts
+++ blank/src/app/app-routing.module.ts
@@ -3,28 +3,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'inicio',
-    loadChildren: () => import('./inicio/inicio.module').then( m => m.InicioPageModule)
-  },
-  {
-    path: 'configuracion',
-    loadChildren: () => import('./configuracion/configuracion.module').then( m => m.ConfiguracionPageModule)
-  },
-  {
-    path: 'tabslinks',
     loadChildren: () => import('./tabslinks/tabslinks.module').then( m => m.TabslinksPageModule)
   },
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
--- blank/src/app/tabslinks/tabslinks-routing.module.ts
+++ blank/src/app/tabslinks/tabslinks-routing.module.ts
@@ -1,13 +1,38 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, ChildActivationEnd } from '@angular/router';
 
 import { TabslinksPage } from './tabslinks.page';
 
 const routes: Routes = [
   {
+    path: 'tabslinks',
+    component: TabslinksPage,
+    children: [
+      {
+        path: 'inicio',
+        loadChildren: () => import('../inicio/inicio.module').then( m => m.InicioPageModule)
+      },
+
+      {
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then( m => m.HomePageModule)
+      },
+      {
+        path: 'configuracion',
+        loadChildren: () => import('../configuracion/configuracion.module').then( m => m.ConfiguracionPageModule)
+      },
+      {
+        path: '',
+        redirectTo:'/tabslinks/inicio',
+        pathMatch: 'full'
+      },
+    ]
+  },
+  {
     path: '',
-    component: TabslinksPage
-  }
+    redirectTo:'/tabslinks/inicio',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
